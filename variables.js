70a5992f871614ca06d5824fef0191bc167e6138
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-15 19:28:00 +0300

    add: file with variables added

---
+++ variables.js
@@ -0,0 +1,22 @@
+//PascalCase - types and classes
+//DB_PASSWORD - constant values
+//camelCase - other variables
+
+//let, const - from ECMAScript 6
+//var - not recommended
+
+let a //variable declaration
+const c = 10 //variable assignment + variable declaration
+a = true //variable assignment
+
+
+//let
+let b = 10
+b = 20 //can change after assignment
+
+let d
+d = false
+
+//const
+const e = 10
+e = 11
