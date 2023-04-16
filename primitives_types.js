38c4b54d591ed2cd63baa042d6a478fb5e467e4d
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-16 16:53:06 +0300

    add: file with primitives types added

---
+++ primitives_types.js
@@ -0,0 +1,8 @@
+//types: string, boolean, number, null, undefined, symbol
+
+const a = 'string' //string
+const b = false //boolean
+const c = 10 //number
+const d = null //null
+let e1; const e2 = undefined //undefined
+let f = Symbol('id') //symbol
