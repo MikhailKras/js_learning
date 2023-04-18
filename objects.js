6c08274501722796525745fc0b4e0e9f85941433
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-18 18:27:20 +0300

    add: file with objects added

---
+++ objects.js
@@ -0,0 +1,45 @@
+const myObject = {
+    name: 'Frank',
+    age: 45,
+    online: true,
+    job: 'truck driver'
+}
+
+const print = (obj) => {
+    console.log(obj)
+}
+
+print(myObject)
+
+myObject.online = 'offline'
+
+myObject['age'] = 52
+
+myObject['height'] = 192
+
+const countryPropertyName = 'country'
+myObject[countryPropertyName] = 'Canada' //only with brackets
+
+print(myObject)
+
+const countryInfo = {
+    continent: 'NA',
+    population: 30
+}
+
+myObject['countryInfo'] = countryInfo
+print(myObject.countryInfo.continent)
+
+
+const name = 'John'
+const surname = 'Farson'
+
+const abbreviatedNote = {
+    name,
+    surname,
+}
+
+
+print(abbreviatedNote)
+
+ 
