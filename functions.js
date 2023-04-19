51a8d16427fc85083616a4dfe95594c7948f7731
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-19 22:20:05 +0300

    add: file with functions added

---
+++ functions.js
@@ -0,0 +1,61 @@
+// first example
+
+function sum(a, b) {
+    const c = a + b
+    console.log(c) // return undefined
+}
+
+sum(1, 2) //3
+sum(10, 20) //30
+
+// function is object
+
+// second example
+
+function myFn(a, b) {
+    let c
+    a = a + 1
+    c = a + b
+    return c // result
+}
+
+// the shortest func
+
+function shortestFunc() {}
+shortestFunc() // undefined
+
+
+// pass by reference (bad practices)
+
+const carOne = {
+    colour: 'green',
+    cost: 200
+}
+
+function increaseCarCost(car) {
+    car.cost += 1
+    return car
+}
+
+increaseCarCost(carOne)
+console.log(carOne.cost) // 201
+
+// pass by reference (best practices)
+
+function increaseCarCostCopy(car) {
+    const updatedCar = Object.assign({}, car)
+    updatedCar.cost += 1
+    return updatedCar
+}
+
+const updatedCarOne = increaseCarCostCopy(carOne)
+console.log(carOne.cost) // 201
+console.log(updatedCarOne.cost) // 202
+
+// callback functions
+
+function printName() { // callback function
+    console.log('Jack')
+}
+
+setTimeout(printName, 10000) // 
