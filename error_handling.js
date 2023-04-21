4c21085e0f7b117c7ceff091cf4b9318882216e7
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-22 02:19:49 +0300

    add: file with error handling added

---
+++ error_handling.js
@@ -0,0 +1,13 @@
+const fnWithError = () => {
+    throw new Error ('Some error')
+}
+
+
+try {
+    // some code
+    fnWithError()
+} catch (error) {
+    // if error in some code
+    console.error(error)
+    // console.log(error.message)
+}
