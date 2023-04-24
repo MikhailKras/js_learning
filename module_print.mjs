7ac150986daf20fc1f039d9046b22f1a92a478d5
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-24 22:44:53 +0300

    add: files with modules (export, import) added

---
+++ module_print.mjs
@@ -0,0 +1,16 @@
+const print = obj => {
+    console.log(obj)
+}
+
+const car = {
+    colour: 'red',
+    size: 'big',
+    cost: 1111
+}
+
+// export default print if one export
+
+export {
+    print,
+    car
+}
---
+++ modules.mjs
@@ -0,0 +1,6 @@
+import {
+    print,
+    car as carOne
+} from './module_print.mjs'
+
+print([123, carOne])
